6fc6be8a0aa38bf780486520ee54dc36254845a9
Kochnov Denis <d.kochnov.optimus@gmail.com>
2021-03-31 10:30:49 +0300

    Refs #24143: add storyblok sync module components

---
+++ components/storyblok/Image.vue
@@ -0,0 +1,11 @@
+<template>
+  <sb-img :src="item.image" :width="1200" :lazy="true" />
+</template>
+
+<script>
+import { Blok } from 'src/modules/vsf-storyblok-module/components'
+export default {
+  name: 'ImageBlok',
+  extends: Blok
+}
+</script>
---
+++ components/storyblok/index.ts
@@ -0,0 +1,8 @@
+import { add } from 'src/modules/vsf-storyblok-module/components'
+
+add('hero', () => import('./Hero.vue'))
+add('Image', () => import('./Image.vue'))
+add('RichText', () => import('./RichText.vue'))
+add('ciLink', () => import('./Link.vue'))
+// Overwrite an existing component
+// add('tile', () => import('./Tile.vue'), { force: true })
