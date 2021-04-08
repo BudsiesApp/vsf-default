1a658f2bbed724ca5975ca7dc8b005afeb734bd1
Kochnov Denis <d.kochnov.optimus@gmail.com>
2021-04-08 17:12:38 +0300

    Refs #24143: tweak empty layout

--- layouts/Empty.vue
+++ layouts/Empty.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="empty-layout">
+  <div class="empty-layout" :class="{'storyblok-preview-mode': isStoryblokPreviewMode}">
     <slot />
   </div>
 </template>
+
+<script>
+
+export default {
+  computed: {
+    isStoryblokPreviewMode () {
+      return this.$route.query.hasOwnProperty('_storyblok')
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="theme/css/main.scss"></style>
+
+<style lang="scss">
+.default-layout {
+  &.storyblok-preview-mode {
+    a {
+      pointer-events: none;
+    }
+  }
+}
+</style>
